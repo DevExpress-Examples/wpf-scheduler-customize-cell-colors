b3d83447cfc8ef9fd5019603d8217ec2b3986ef3
DevExpressExampleBot <fakeEmail>
2023-11-20 18:33:08 +0400

    Source auto update [skip ci]

--- VB/CustomCellColoring/Model/MyCategory.vb
+++ VB/CustomCellColoring/Model/MyCategory.vb
@@ -1,22 +1,27 @@
-﻿Imports DevExpress.Mvvm.POCO
+Imports DevExpress.Mvvm.POCO
 Imports System.Windows.Media
 
 Namespace CustomCellColoring.Model
+
     Public Class MyCategory
-            Protected Sub New(ByVal id As Integer, ByVal name As String, ByVal color As Color)
-                Me.Id = id
-                Me.Name = name
-                ColorInfo = color
-            End Sub
-            Public Shared Function Create(ByVal id As Integer, ByVal name As String, ByVal color As Color) As MyCategory
-                Return ViewModelSource.Create(Function() New MyCategory(id, name, color))
-            End Function
 
-            Protected Sub New()
-            End Sub
+        Protected Sub New(ByVal id As Integer, ByVal name As String, ByVal color As Color)
+            Me.Id = id
+            Me.Name = name
+            ColorInfo = color
+        End Sub
 
-            Public Overridable Property Id() As Integer
-            Public Overridable Property Name() As String
-            Public Overridable Property ColorInfo() As Color
+        Public Shared Function Create(ByVal id As Integer, ByVal name As String, ByVal color As Color) As MyCategory
+            Return ViewModelSource.Create(Function() New MyCategory(id, name, color))
+        End Function
+
+        Protected Sub New()
+        End Sub
+
+        Public Overridable Property Id As Integer
+
+        Public Overridable Property Name As String
+
+        Public Overridable Property ColorInfo As Color
     End Class
 End Namespace
--- VB/CustomCellColoring/ViewModels/MyViewModel.vb
+++ VB/CustomCellColoring/ViewModels/MyViewModel.vb
@@ -1,17 +1,15 @@
-﻿Imports CustomCellColoring.Model
+Imports CustomCellColoring.Model
 Imports DevExpress.Mvvm.DataAnnotations
-Imports System
-Imports System.Collections.Generic
 Imports System.Collections.ObjectModel
-Imports System.Linq
-Imports System.Text
-Imports System.Threading.Tasks
 Imports System.Windows.Media
 
 Namespace CustomCellColoring.ViewModels
-    <POCOViewModel> _
+
+    <POCOViewModel>
     Public Class MyViewModel
-        Public Overridable Property Categories() As ObservableCollection(Of MyCategory)
+
+        Public Overridable Property Categories As ObservableCollection(Of MyCategory)
+
         Protected Sub New()
             Categories = CreateCategories()
             UseCustomCellControlStyle = True
@@ -19,18 +17,20 @@ Namespace CustomCellColoring.ViewModels
             UseResourceColors = True
         End Sub
 
-        #Region "#CreateCategories"
+#Region "#CreateCategories"
         Private Function CreateCategories() As ObservableCollection(Of MyCategory)
-            Dim result As New ObservableCollection(Of MyCategory)()
+            Dim result As ObservableCollection(Of MyCategory) = New ObservableCollection(Of MyCategory)()
             result.Add(MyCategory.Create(0, "White category", Colors.White))
             result.Add(MyCategory.Create(1, "Orange category", Colors.Orange))
             result.Add(MyCategory.Create(2, "Red category", Colors.Red))
             Return result
         End Function
-        #End Region ' #CreateCategories
 
-        Public Overridable Property UseResourceBrushSchema() As Boolean
-        Public Overridable Property UseResourceColors() As Boolean
-        Public Overridable Property UseCustomCellControlStyle() As Boolean
+#End Region  ' #CreateCategories
+        Public Overridable Property UseResourceBrushSchema As Boolean
+
+        Public Overridable Property UseResourceColors As Boolean
+
+        Public Overridable Property UseCustomCellControlStyle As Boolean
     End Class
 End Namespace
